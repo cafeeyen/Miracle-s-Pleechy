fa1444cf0b8a7c631b343e9b53f9b5e58fc6a24e
cafeeyen <s7070113@kmitl.ac.th>
2014-12-12 21:15:49 +0700

    Add:pic when sheep jump Fix:time select window
    Change select time from new window to in main window.
    By change start button to 1 mins and quit button to 2 mins.

--- pleechy.py
+++ pleechy.py
@@ -9,9 +9,11 @@ name = """
     By Chunyanuch 57070027 & Suttinee 57070113
     """
 
+state = 0
 score = 0
 timer = 0
 logo = PhotoImage(file="banner.gif")#edit position here
+sheep = PhotoImage(file="sheep jump/%d.gif" % state)
 
 wlogo = Label(root, image=logo).grid(row=0)
 wname = Label(root, text=name).grid(row=1)
@@ -25,25 +27,26 @@ wtime.grid(row=3)
 wask = Label(root, padx = 10, text=' ')
 wask.grid(row=4)
 
+wgame = Label(root, image=sheep)
+wgame.grid(row=5)
 
-def start_time(sec, top):
+
+def start_time(sec):
     """Start timer"""
     #Can't show how many time left
-    Quit(top)
     global timer
     timer = sec
     tick()
-    start.config(text='Sent', command=check)
+    sheep.config(file="sheep jump/1.gif")
+    bnt_1.config(text='Sent', command=check)
+    bnt_2.config(text='Quit', command=root.destroy)
     question()
     
 
 def set_time():
-    top = Toplevel()
-    lbl = Label(top, text="Choose Time").grid(row=0)
-    t_1 = Button(top, text='1 Minute', width=50,
-                 command=lambda: start_time(60, top)).grid(row=0)
-    t_2 = Button(top, text='2 Minute', width=50,
-                 command=lambda: start_time(120, top)).grid(row=1)
+    lbl = Label(text="Choose Time")
+    bnt_1.config(text='1 Minute', command=lambda: start_time(60))
+    bnt_2.config(text='2 Minute', command=lambda: start_time(120))
 
 
 def tick():
@@ -85,59 +88,39 @@ def one():
     global score
     score += 1
     wscore.config(text=explanation+str(score))
-    #Credit art ascii from http://www.heartnsoul.com/ascii_art/sheep.txt
-    #http://textart4u.blogspot.com/2013/05/game-over-text-art.html
-    print """
-
-        .-:\'  `; `-._ 
-       (_,           )
-     ,\'o"(            )>
-    (__,-\'            )
-     (                 )
-      `-\'._.------._.-\'
-         ///       ///'
-      ___________________
-     |__ ::___::___::___|
-        |  | |  | |  |'
-        
-    """
-    print '\t%d SHEEP JUMP!' % score
+    jump()
     question()
+
+
+def jump():
+    """Sheep jump when correct"""
+    global state
+    sheep.config(file="sheep jump/%d.gif" % state)
+    state += 1
+    if state == 6:
+        state = 1
+        sheep.config(file="sheep jump/%d.gif" % state)
+        return 0
+    wgame.after(100, jump)
+    
     
 
 def two():
-    print """
-
-    ███▀▀▀██┼███▀▀▀███┼███▀█▄█▀███┼██▀▀▀
-    ██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼█┼┼┼██┼██┼┼┼
-    ██┼┼┼▄▄▄┼██▄▄▄▄▄██┼██┼┼┼▀┼┼┼██┼██▀▀▀
-    ██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██┼┼┼
-    ███▄▄▄██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██▄▄▄
-
-
-    ███▀▀▀███┼▀███┼┼██▀┼██▀▀▀┼██▀▀▀▀██▄┼
-    ██┼┼┼┼┼██┼┼┼██┼┼██┼┼██┼┼┼┼██┼┼┼┼┼██┼
-    ██┼┼┼┼┼██┼┼┼██┼┼██┼┼██▀▀▀┼██▄▄▄▄▄▀▀┼
-    ██┼┼┼┼┼██┼┼┼██┼┼█▀┼┼██┼┼┼┼██┼┼┼┼┼██┼
-    ███▄▄▄███┼┼┼─▀█▀┼┼─┼██▄▄▄┼██┼┼┼┼┼██▄
-    """
-    global score, timer
+    sheep.config(file="game over/0.gif")
+    global score, timer, state
     timer = '-'
     tick()
-    score = 0
-    start.config(text='Start Game', command=set_time)
+    score, state = 0, 0
+    bnt_1.config(text='Start Game', command=set_time)
     
-
-def Quit(window):
-    window.destroy()
     
 ansbox = Entry(width=60)
-ansbox.grid(row=5)
+ansbox.grid(row=6)
 
-start = Button(text='Start Game', width=50, command=set_time)
-start.grid(row=6)
+bnt_1 = Button(text='Start Game', width=50, command=set_time)
+bnt_1.grid(row=7)
 
-out = Button(text='Quit', width=50, command=lambda: Quit(root))
-out.grid(row=7)
+bnt_2 = Button(text='Quit', width=50, command=root.destroy)
+bnt_2.grid(row=8)
 
 root.mainloop()
