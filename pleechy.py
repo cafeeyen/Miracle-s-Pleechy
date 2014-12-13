de67c0340895801cfc7588f37f272446eb8c3450
cafeeyen <s7070113@kmitl.ac.th>
2014-12-13 23:24:16 +0700

    Add:Unlimit time mode. Change:Pic when sheep jump.

--- pleechy.py
+++ pleechy.py
@@ -5,46 +5,47 @@ root = Tk()
 root.title('Miracle\'s Pleechy')
 
 explanation = """ Your score  :  """
-name = """
-    By Chunyanuch 57070027 & Suttinee 57070113
-    """
 
 state = 0
 score = 0
 timer = 0
-logo = PhotoImage(file="banner.gif")#edit position here
-sheep = PhotoImage(file="sheep jump/%d.gif" % state)
+gamebg = PhotoImage(file="pic/main.gif")
 
-wlogo = Label(root, image=logo).grid(row=0)
-wname = Label(root, text=name).grid(row=1)
+wgame = Label(root, image=gamebg)
+wgame.grid(row=0)
 
 wscore = Label(root, padx = 10, text=explanation+'0')
-wscore.grid(row=2)
+wscore.grid(row=1)
 
 wtime = Label(root, padx = 10, text='Time : -')
-wtime.grid(row=3)
+wtime.grid(row=2)
 
 wask = Label(root, padx = 10, text=' ')
-wask.grid(row=4)
-
-wgame = Label(root, image=sheep)
-wgame.grid(row=5)
+wask.grid(row=3)
 
 
 def start_time(sec):
     """Start timer"""
     #Can't show how many time left
-    global timer
+    bnt_0.destroy()
+    gamebg.config(file="pic/jump0.gif")
+    global timer, ansbox
+    ansbox = Entry(width=60)
+    ansbox.grid(row=4)
     timer = sec
     tick()
-    sheep.config(file="sheep jump/1.gif")
+    wscore.config(text=explanation+str(score))
     bnt_1.config(text='Sent', command=check)
     bnt_2.config(text='Quit', command=root.destroy)
     question()
     
 
 def set_time():
-    lbl = Label(text="Choose Time")
+    global bnt_0
+    wask.config(text="Choose Time")
+    bnt_0 = Button(text='Unlimited', width=50,
+                   command=lambda: start_time('Unlimited'))
+    bnt_0.grid(row=4)
     bnt_1.config(text='1 Minute', command=lambda: start_time(60))
     bnt_2.config(text='2 Minute', command=lambda: start_time(120))
 
@@ -95,32 +96,35 @@ def one():
 def jump():
     """Sheep jump when correct"""
     global state
-    sheep.config(file="sheep jump/%d.gif" % state)
+    gamebg.config(file="pic/jump%d.gif" % state)
     state += 1
-    if state == 6:
-        state = 1
-        sheep.config(file="sheep jump/%d.gif" % state)
+    if state == 5:
+        state = 0
+        gamebg.config(file="pic/jump%d.gif" % state)
         return 0
-    wgame.after(100, jump)
+    wgame.after(75, jump)
     
     
 
 def two():
-    sheep.config(file="game over/0.gif")
     global score, timer, state
+    ansbox.destroy()
+    gamebg.config(file="pic/main.gif")
     timer = '-'
     tick()
     score, state = 0, 0
+    wask.config(text='%d + %d = %d' % (num_a, num_b, num_a + num_b))
     bnt_1.config(text='Start Game', command=set_time)
     
+
+def Quit(window):
+    window.destroy()
     
-ansbox = Entry(width=60)
-ansbox.grid(row=6)
 
 bnt_1 = Button(text='Start Game', width=50, command=set_time)
-bnt_1.grid(row=7)
+bnt_1.grid(row=5)
 
 bnt_2 = Button(text='Quit', width=50, command=root.destroy)
-bnt_2.grid(row=8)
+bnt_2.grid(row=6)
 
 root.mainloop()
